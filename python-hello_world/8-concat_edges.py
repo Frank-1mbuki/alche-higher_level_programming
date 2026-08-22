9a83d1646799dd5f954c928fb58fc440500d413a
Frank-1mbuki <frankimbuki03@gmail.com>
2026-08-22 06:35:26 -0700

    Task 8. Create a new sentence

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+words = str.split()
+str = "+ words[5] +" " + words[6] + " "with" " + words[0] +"
+print(str)
